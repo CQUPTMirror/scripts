6357c909948f0b7b85ba105ca13932b94ddeab4c
zfox49 <zfox49@users.noreply.github.com>
2016-03-30 14:01:13 +0800

    Update archlinux.sh
    更换上游到Leaseweb

--- rsync/archlinux.sh
+++ rsync/archlinux.sh
@@ -1,1 +1,1 @@
-rsync --archive --links --hard-links --times --verbose --delete --recursive --exclude .~tmp~/ rsync://ftp.tku.edu.tw/archlinux/ /data/mirror/archlinux >> /var/log/rsync/archlinux.log-$(date "+%Y-%m-%d")
+rsync --archive --links --hard-links --times --verbose --delete --recursive --exclude .~tmp~/ rsync://mirror.nl.leaseweb.net/archlinux/ /data/mirror/archlinux >> /var/log/rsync/archlinux.log-$(date "+%Y-%m-%d")
