17e514f3ccd5e19c63e708b44a4f04caea75b62c
zfox49 <zfox49@users.noreply.github.com>
2016-04-04 12:55:48 +0800

    Update pypi.sh

--- rsync/pypi.sh
+++ rsync/pypi.sh
@@ -1,1 +1,1 @@
-rsync --archive --links --hard-links --times --verbose --delete --recursive --exclude .~tmp~/ mirrors.ustc.edu.cn::pypi/ /data/mirror/pypi >> /var/log/rsync/pypi.log-$(date "+%Y-%m-%d")
+rsync --archive --links --hard-links --times --verbose --delete --recursive --exclude .~tmp~/ --delete-excluded mirrors.ustc.edu.cn::pypi/ /data/mirror/pypi >> /var/log/rsync/pypi.log-$(date "+%Y-%m-%d")
