ac0e049739f56423ba0a842491bafbfc13c6d3e2
zfox49 <zfox49@users.noreply.github.com>
2016-04-04 12:55:10 +0800

    Update archlinuxcn.sh

--- rsync/archlinuxcn.sh
+++ rsync/archlinuxcn.sh
@@ -1,1 +1,1 @@
-rsync --archive --links --hard-links --times --verbose --delete --recursive --exclude .~tmp~/ mirrors.ustc.edu.cn::archlinuxcn/ /data/mirror/archlinuxcn >> /var/log/rsync/archlinuxcn.log-$(date "+%Y-%m-%d")
+rsync --archive --links --hard-links --times --verbose --delete --recursive --exclude .~tmp~/ --delete-excluded mirrors.ustc.edu.cn::archlinuxcn/ /data/mirror/archlinuxcn >> /var/log/rsync/archlinuxcn.log-$(date "+%Y-%m-%d")
