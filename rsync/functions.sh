7e008f369d55b57a6a2f2f83f63a1fbdcf5862f7
Zhang Rui <nanjingzr@yahoo.com>
2016-05-16 15:59:07 +0800

    fix(functions.sh): redirect output of chown to logs

--- rsync/functions.sh
+++ rsync/functions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## This file defines functions that is used in a sync process.
 
-detect-target() {
 ## This function detects whether the target directory exists. If not, it will exit with an error code -1.
 ## Usage: detect-target $TARGET_NAME;
+detect-target() {
     local TARGET_NAME="$1";
     local LOCAL_PATH="/data/mirror/$TARGET_NAME";
 
@@ -13,9 +13,9 @@ detect-target() {
     fi
 }
 
-mirror-fetch-with-rsync() {
 ## This function do the actual process with rsync protocol.
 ## Usage: mirror-fetch-with-rsync $TARGET_NAME $TARGET_URL
+mirror-fetch-with-rsync() {
     local TARGET_NAME="$1";
     local TARGET_URL="$2";
     local LOCAL_DATE;
@@ -40,9 +40,9 @@ mirror-fetch-with-rsync() {
         >> "$LOCAL_RSYNC_LOGGER"; 
 }
 
-set-permission() {
 ## This function sets permission of all files and directories in the certain path with 755 for directories and 644 for files.
 ## Usage: set-permission $TARGET_NAME 
+set-permission() {
     local TARGET_NAME="$1";
     local LOCAL_PATH="/data/mirror/$TARGET_NAME";
     local LOCAL_DATE;
@@ -50,14 +50,14 @@ set-permission() {
     # chperm is short for change permission.
     local LOCAL_CHMOD_LOGGER="/var/log/rsync/$TARGET_NAME-chperm-$LOCAL_DATE.log";
 
-    chown -R www-data:www-data "$LOCAL_PATH";
+    chown -R -c www-data:www-data "$LOCAL_PATH" >> "$LOCAL_CHMOD_LOGGER";
     find "$LOCAL_PATH" -type d -print0 | xargs --null chmod -c 755 >> "$LOCAL_CHMOD_LOGGER";
     find "$LOCAL_PATH" -type f -print0 | xargs --null chmod -c 644 >> "$LOCAL_CHMOD_LOGGER";
 }
 
-fetch() {
 ## This function does a full process of sync. 
 ## Usage: fetch $PROTOCOL $TARGET_NAME $TARGET_URL
+fetch() {
     local TYPE="$1";
     local TARGET_NAME="$2";
     local TARGET_URL="$3";
