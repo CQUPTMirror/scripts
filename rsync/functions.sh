980e23e9881278be676fb1acb58b968ad8c41012
Zhang Rui <nanjingzr@yahoo.com>
2016-05-16 15:32:40 +0800

    style(functions.sh): use -c instead of -v in set-permission function
    chmod -c and chown -c will only output file mode change, which shorten
    outputs.
    Furthermore, comments have been added to the file, including function
    description and usage.

--- rsync/functions.sh
+++ rsync/functions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+## This file defines functions that is used in a sync process.
+
 detect-target() {
+## This function detects whether the target directory exists. If not, it will exit with an error code -1.
+## Usage: detect-target $TARGET_NAME;
     local TARGET_NAME="$1";
     local LOCAL_PATH="/data/mirror/$TARGET_NAME";
 
@@ -10,6 +14,8 @@ detect-target() {
 }
 
 mirror-fetch-with-rsync() {
+## This function do the actual process with rsync protocol.
+## Usage: mirror-fetch-with-rsync $TARGET_NAME $TARGET_URL
     local TARGET_NAME="$1";
     local TARGET_URL="$2";
     local LOCAL_DATE;
@@ -35,18 +41,23 @@ mirror-fetch-with-rsync() {
 }
 
 set-permission() {
+## This function sets permission of all files and directories in the certain path with 755 for directories and 644 for files.
+## Usage: set-permission $TARGET_NAME 
     local TARGET_NAME="$1";
     local LOCAL_PATH="/data/mirror/$TARGET_NAME";
     local LOCAL_DATE;
     LOCAL_DATE=$(date '+%Y-%m-%d');
-    local LOCAL_CHMOD_LOGGER="/var/log/rsync/$TARGET_NAME-chmod-$LOCAL_DATE.log";
+    # chperm is short for change permission.
+    local LOCAL_CHMOD_LOGGER="/var/log/rsync/$TARGET_NAME-chperm-$LOCAL_DATE.log";
 
     chown -R www-data:www-data "$LOCAL_PATH";
-    find "$LOCAL_PATH" -type d -print0 | xargs --null chmod -v 755 | grep -v "changed from 0775 (rwxrwxr-x) to 0755 (rwxr-xr-x)" >> "$LOCAL_CHMOD_LOGGER";
-    find "$LOCAL_PATH" -type f -print0 | xargs --null chmod -v 644 | grep -v "changed from 0664 (rw-rw-r--) to 0644 (rw-r--r--)" >> "$LOCAL_CHMOD_LOGGER";
+    find "$LOCAL_PATH" -type d -print0 | xargs --null chmod -c 755 >> "$LOCAL_CHMOD_LOGGER";
+    find "$LOCAL_PATH" -type f -print0 | xargs --null chmod -c 644 >> "$LOCAL_CHMOD_LOGGER";
 }
 
 fetch() {
+## This function does a full process of sync. 
+## Usage: fetch $PROTOCOL $TARGET_NAME $TARGET_URL
     local TYPE="$1";
     local TARGET_NAME="$2";
     local TARGET_URL="$3";
