ddab99acda2236dc0e9feb5e8436edabc1e13425
Zhang Rui <nanjingzr@yahoo.com>
2016-07-09 13:05:51 +0800

    fix(anthonos.sh): change repo sync url

--- rsync/anthonos.sh
+++ rsync/anthonos.sh
@@ -2,6 +2,6 @@
 . "$(dirname "$0")/functions.sh";
 
 TARGET_NAME='anthonos';
-TARGET_URL="rsync://repo.anthonos.org/";
+TARGET_URL="rsync://mirror.anthonos.org/";
 
 fetch 'rsync' "$TARGET_NAME" "$TARGET_URL";
