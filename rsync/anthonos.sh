f3c672e2c583a432d006d01d838073cbdfda63eb
Zhang Rui <nanjingzr@yahoo.com>
2016-07-09 13:20:26 +0800

    fix(anthonos.sh): change sync url for AOSC OS repo

--- rsync/anthonos.sh
+++ rsync/anthonos.sh
@@ -2,6 +2,6 @@
 . "$(dirname "$0")/functions.sh";
 
 TARGET_NAME='anthonos';
-TARGET_URL="rsync://mirror.anthonos.org/";
+TARGET_URL="rsync://mirror.anthonos.org::anthon";
 
 fetch 'rsync' "$TARGET_NAME" "$TARGET_URL";
