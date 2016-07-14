7d7ab41b28c1428320c1bcb4bd9c438ef49edeed
Zhang Rui <nanjingzr@yahoo.com>
2016-07-15 00:12:39 +0800

    fix(anthonos.sh): change file path of repo for AOSC

--- rsync/anthonos.sh
+++ rsync/anthonos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . "$(dirname "$0")/functions.sh";
 
-TARGET_NAME='anthonos';
+TARGET_NAME='anthon';
 TARGET_URL="rsync://mirror.anthonos.org/anthon";
 
 fetch 'rsync' "$TARGET_NAME" "$TARGET_URL";
