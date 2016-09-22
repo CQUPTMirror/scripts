b1c8c8659926779686066c62123d581d115527e9
Zhang Rui <nanjingzr@yahoo.com>
2016-09-23 00:16:12 +0800

    hotfix: change upstream for ubuntu-releases

--- rsync/ubuntu-releases.sh
+++ rsync/ubuntu-releases.sh
@@ -2,6 +2,6 @@
 . "$(dirname "$0")/functions.sh";
 
 TARGET_NAME='ubuntu-releases';
-TARGET_URL="rsync://mirrors.aliyun.com/$TARGET_NAME/";
+TARGET_URL="rsync://mirror.ams1.nl.leaseweb.net/$TARGET_NAME/";
 
 fetch 'rsync' "$TARGET_NAME" "$TARGET_URL";
